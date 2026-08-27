469b3f7a34dd800723902ffe23b28c3f46dda12b
sijeong <dnekd4@gmail.com>
2026-08-27 12:00:31 +0900

    feat: 백엔드 모듈러 모놀리스 구조 세팅 및 DB 연결

---
+++ app/local_info/router.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/local-info", tags=["local_info"])
---
+++ app/member/router.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/members", tags=["member"])
